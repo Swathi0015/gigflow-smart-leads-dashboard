319eee622ede4d5231f74b9a257f6eb90fe3bf54
Swathi Niddena <swathiniddena@gmail.com>
2026-05-19 19:54:11 +0530

    Final Render deployment fix

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,6 @@
 import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 import cors from "cors";
-
-import authRoutes from "./routes/authRoutes";
-import leadRoutes from "./routes/leadRoutes";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -13,21 +9,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
 
-// ROUTES
-app.use("/api/auth", authRoutes);
-app.use("/api/leads", leadRoutes);
+const PORT = process.env.PORT || 5000;
 
-
-mongoose
-  .connect(process.env.MONGO_URI as string)
-  .then(() => {
-    console.log("MongoDB Connected");
-
-    app.listen(5000, () => {
-      console.log("Server running on port 5000");
-    });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
